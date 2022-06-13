20fd3494471273dfbdedde2ace9c9418b71156dc
James Dunne <james.jdunne@gmail.com>
2022-06-13 14:44:49 -0500

    Added independent tool to emit RetroArch WRITE_CORE_MEMORY commands to emit the test drawlist code

---
+++ tool_emitdrawlist.cpp
@@ -0,0 +1,57 @@
+#include <stdint.h>
+#include <stdio.h>
+#include "drawlist_fwd.hpp"
+
+using namespace DrawList;
+
+static uint16_t cmd[] = {
+    3, CMD_TARGET, OAM, 3,
+    // BG2 H/V offset relative:
+    2, CMD_BG_OFFSET, 0x0808,
+    // Position at upper-left of overworld Link's house:
+    3, CMD_XY_OFFSET, 2048, 2560,
+    3, CMD_COLOR_DIRECT_BGR555, COLOR_STROKE, 0x1F3F,
+    3, CMD_PIXEL, 18, 118,
+    9, CMD_IMAGE, 20, 120, 2, 2,
+    0x001F, 0x03E0,
+    0x7C00, 0x001F,
+    3, CMD_COLOR_DIRECT_BGR555, COLOR_STROKE, 0x7FFF,
+    4, CMD_HLINE, 24, 124, 16,
+    4, CMD_VLINE, 24, 124, 16,
+    3, CMD_COLOR_DIRECT_BGR555, COLOR_STROKE, 0x7C00,
+    5, CMD_RECT, 32, 132, 8, 8,
+    3, CMD_COLOR_DIRECT_BGR555, COLOR_FILL, 0x001F,
+    5, CMD_RECT_FILL, 38, 138, 16, 16,
+    4, CMD_COLOR_PALETTED, COLOR_FILL, 0, 0xF0,
+    5, CMD_RECT_FILL, 80, 0, 4, 4,
+    4, CMD_COLOR_PALETTED, COLOR_FILL, 0, 0xF1,
+    5, CMD_RECT_FILL, 84, 0, 4, 4,
+    4, CMD_COLOR_PALETTED, COLOR_FILL, 0, 0xF2,
+    5, CMD_RECT_FILL, 88, 0, 4, 4,
+    4, CMD_COLOR_PALETTED, COLOR_FILL, 0, 0xF3,
+    5, CMD_RECT_FILL, 92, 0, 4, 4,
+    5, CMD_COLOR_DIRECT_BGR555, COLOR_STROKE, 0x1C4E, COLOR_OUTLINE, 0x0C27,
+    // -23
+    5, CMD_LINE, static_cast<uint16_t>(-10), static_cast<uint16_t>(-12), 260, 239,
+    // -17
+    7, CMD_COLOR_DIRECT_RGB888, COLOR_STROKE, 0x0000, 0xFF00, COLOR_OUTLINE, 0x00FF, 0x0000,
+    // -9
+    8, CMD_TEXT_UTF8, 0, 0,
+    7, 0, 0, 0, 0,
+};
+#define cmd_len (sizeof(cmd) / sizeof(uint16_t))
+
+int main(void) {
+    uint32_t s = sizeof(cmd);
+    printf("WRITE_CORE_MEMORY %lX", (uint32_t) 0xFFFE0000);
+    for (int i = 0; i < s; i++) {
+        auto c = ((uint8_t*)cmd)[i];
+        printf(" %02X", c);
+    }
+    printf("\n");
+
+    // write size in 4 bytes:
+    printf("WRITE_CORE_MEMORY %lX", (uint32_t) 0xFFFFFFFC);
+    printf(" %02X %02X %02X %02X", s & 0xFF, (s >> 8) & 0xFF, (s >> 16) & 0xFF, (s >> 24) & 0xFF);
+    printf("\n");
+}
